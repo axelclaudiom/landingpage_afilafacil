0f45dc4391bc7f59af683d54bea6798dc5eee542
Axel Claudio Melgar <axelclaudiom@gmail.com>
2025-05-12 14:14:20 -0300

    added javascript

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
-<script>
-  const images = document.querySelectorAll('.slider img');
-  let currentIndex = 0;
+const images = document.querySelectorAll('.slider img');
+let currentIndex = 0;
 
-  // Oculta todas las imágenes excepto la primera
-  for (let i = 0; i < images.length; i++) {
-    if (i === 0) {
-      images[i].style.display = 'block';
-    } else {
-      images[i].style.display = 'none';
-    }
-  }
-
-  setInterval(function() {
-    images[currentIndex].style.display = 'none';
-    currentIndex = (currentIndex + 1) % images.length;
-    images[currentIndex].style.display = 'block';
-  }, 3000);
-</script>
+setInterval(() => {
+  images[currentIndex].style.display = 'none'; // Oculta la imagen actual
+  currentIndex = (currentIndex + 1) % images.length; // Pasa a la siguiente imagen
+  images[currentIndex].style.display = 'block'; // Muestra la nueva imagen
+}, 3000); // Cambia cada 3 segundos
